13d8b90dbdbae0b8c6052152f961ca2697542971
Arcadia101 <camitu16@hotmail.com>
2024-02-28 16:35:06 -0500

    fix materials

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using KBCore.Refs;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Platformer
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Enemy : Entity
+    {
+        [SerializeField, Self] private NavMeshAgent agent;
+        [SerializeField, Child] private Animator animator;
+
+        private StateMachine stateMachine;
+
+        private void OnValidate() => this.ValidateRefs();
+
+        private void Start()
+        {
+            stateMachine = new StateMachine();
+        }
+        
+        void At(IState from, IState to, IPredicate condition) => stateMachine.AddTransition(from, to, condition);
+        void Any(IState to, IPredicate condition) => stateMachine.AddAnyTransition(to, condition);
+
+        private void Update()
+        {
+            stateMachine.Update();
+        }
+
+        private void FixedUpdate()
+        {
+            stateMachine.FixedUpdate();
+        }
+    }
+}
